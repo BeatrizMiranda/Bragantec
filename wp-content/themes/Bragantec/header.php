bc3a710f2ac86e52aa66c60eae065a8c34398e17
Bia <biia.mfs@gmail.com>
2019-03-28 13:44:28 -0300

    comit inicial

---
+++ wp-content/themes/Bragantec/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+	  <title>Bragantec</title>
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content ="">
+		<meta name="keywords" content="">
+
+
+    <!-- favicon na aba do navegador -->
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri();?>/img/favicon.ico">
+
+    <!-- para deixar a aba no  mobile colorida -->
+  	<meta name="theme-color" content="#7ca7db">
+  	<meta name="apple-mobile-web-app-status-bar-style" content="#7ca7db">
+  	<meta name="msapplication-navbutton-color" content="#7ca7db">
+
+    <!--Bootstrap-->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <!--Css-->
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/style.css" type="text/css" />
+
+  </head>
+
+  <body>
+
+
+		<nav class="navbar navbar-expand-lg navbar-light bg-light menuPrincipal">
+		  <a class="navbar-brand" href="#">
+				<img src="<?php echo get_stylesheet_directory_uri();?>/img/lampada.png" width="30" height="30" alt="">
+				Bragantec
+			</a>
+
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+			<div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+		    <ul class="navbar-nav">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="<?php echo get_site_url();?>">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item" data-toggle="tooltip" data-placement="bottom" title="Em breve mais informações">
+		         <a class="nav-link disabled" href="#">Como Participar</a>
+		      </li>
+		    </ul>
+		  </div>
+		</nav>
