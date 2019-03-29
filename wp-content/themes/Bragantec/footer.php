71467f7d010df7145b71b1f8fcf200bc0d484287
Bia <biia.mfs@gmail.com>
2019-03-29 14:43:02 -0300

    footer

--- wp-content/themes/Bragantec/footer.php
+++ wp-content/themes/Bragantec/footer.php
@@ -1,3 +1,76 @@
+    <footer class="container-fluid">
+      <div class="row">
+
+        <div class="fundoVer text-center col-md-12">
+            <h2>Bragantec</h2>
+        </div>
+
+        <div class="col-md-12 row tituloFooter">
+          <h2 class="col-md-8 text-center">Contato:</h2>
+          <h2 class="col-md-4text-center">O site foi desenvolvido por:</h2>
+        </div>
+        <div class="row col-md-12 contatoDesenv">
+          <div class="col-md-4 row contato">
+            <img class="col-md-3" src="<?php echo get_stylesheet_directory_uri()?>/img/lampada.png" alt="informaçoes da bragantec" width="20" height="70">
+            <div class="col-md-9">
+              Facebook: <a href="facebook.com/bragantec" target="_blank"> facebook.com/bragantec</a><br>
+              E-mail: <a href="mailto:" target="_blank">bragantec@bragantec.com.br</a>
+            </div>
+          </div>
+          <div class="col-md-4 row contato">
+            <img class="col-md-3" src="<?php echo get_stylesheet_directory_uri()?>/img/if.png" alt="informaçoes da bragantec" width="40" height="65">
+            <div class="col-md-9">
+              Telefone: <a href="" target="_blank">(11) xxxx-xxxx</a><br>
+              E-mail: <a href="mailto:" target="_blank">ex@ifsp.com.br</a><br>
+              Facebook: <a href="facebook.com/ifsp" target="_blank"> facebook.com/ifsp</a><br>
+            </div>
+          </div>
+          <div class="col-md-4 row text-center">
+            <div class="col-md-6 desenvolvedoras">
+              <div class="col-md-12">
+                  <img src="<?php echo get_stylesheet_directory_uri()?>/img/beatriz.jpg" alt="Beatriz Miranda">
+              </div>
+              <div class="row">
+                <p class="col-md-12">Beatriz Miranda <br> Programadora</p>
+              </div>
+            </div>
+            <div class="col-md-6 desenvolvedoras">
+              <div class="col-md-12">
+                  <img src="<?php echo get_stylesheet_directory_uri()?>/img/laura.jpg" alt="Laura Chaile">
+              </div>
+              <div class="row">
+                <p class="col-md-12">Laura Chaile <br> Designer</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row col-md-12 apoios">
+          <div class="col-md-6 row ">
+            <img class="col-md-3" src="<?php echo get_stylesheet_directory_uri()?>/img/ifMeninas.png" alt="Logo if Meninas" width="20" height="120">
+            <div class="col-md-9">
+              <p>Esse site foi desenvolvido em parceria com o IFMeninas, grupo que apoia meninas na computação. </p><br>
+              <img src="<?php echo get_stylesheet_directory_uri();?>/img/web.svg" width="25" alt="icone referente ao site if Meninas"/>
+              <a href="bra.ifsp.edu.br/ifmeninas/" target="_blank">bra.ifsp.edu.br/ifmeninas/</a>
+            </div>
+          </div>
+          <div class="col-md-6 row ">
+            <p class="col-md-12">Bragantec é apoiada por: </p>
+            <br>
+            <div class="col-md-12 text-center">
+              <img class="cnpq" src="<?php echo get_stylesheet_directory_uri()?>/img/cnpq.png" alt="logo cnpq">
+            </div>
+          </div>
+        </div>
+
+        <div class="fundoVer text-right col-md-12">
+          2019
+        </div>
+      </div>
+
+    </footer>
+
+
     <!-- Links -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
--- wp-content/themes/Bragantec/index.php
+++ wp-content/themes/Bragantec/index.php
@@ -2,8 +2,12 @@
 
 <section class="container">
   <div class="row">
-    <div class="col-md-6 atualizacoes">
-      <h1>Receber atualizações: </h1>
+    <div class="col-md-6 col-xs-12 atualizacoes">
+      <h2>
+        <img width="20" src="<?php echo get_stylesheet_directory_uri();?>/img/seta.png" alt="">
+        Receber atualizações:
+      </h2>
+
       <form id="formulario" action="" method="post">
         <div class="form-group">
           <input type="email" class="form-control" name="email" id="email" placeholder="Email" required="required">
@@ -11,7 +15,7 @@
         <button id="botaoFormulario" type="submit"  name="cadastro" class="btn btn-primary botao">Me Notifique</button>
       </form>
     </div>
-    <div class="col-md-6" >
+    <div class="col-md-6 col-xs-12">
       <img id="imgConstrucao" src="<?php echo get_stylesheet_directory_uri();?>/img/siteConstrucao.png" alt="Site em Construção">
     </div>
   </div>
