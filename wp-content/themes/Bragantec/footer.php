eeed26c540a2be9f2a29eab203253f629d83bfd6
Bia <biia.mfs@gmail.com>
2019-04-04 13:36:51 -0300

    arrumei o footer mobile

--- wp-content/themes/Bragantec/footer.php
+++ wp-content/themes/Bragantec/footer.php
@@ -12,31 +12,27 @@
 
         <div class="row col-md-12 contatoDesenv">
 
-          <div class="col-md-4 col-xs-12 row contato">
-            <div class="col-md-3 col-xs-3 text-center" style="padding-top: 15px;">
-              <img class="img" src="<?php echo get_stylesheet_directory_uri()?>/img/icone2_site.jpg" alt="informaçoes da bragantec" width="60">
-            </div>
-            <div class="col-md-9 col-xs-9">
-              Bragantec: <br>
+          <div class="col-md-4 row contato braga">
+            Contato Bragantec:
+            <div class="contatoDivBraga">
               Facebook: <a href="https://www.facebook.com/bragantec/" target="_blank"> facebook.com/bragantec</a><br>
               E-mail: <a href="mailto:bragantec@ifsp.edu.br" target="_blank">bragantec@ifsp.edu.br</a>
             </div>
           </div>
 
           <div class="col-md-4 row contato">
-            <img class="col-md-3 col-xs-12 img" src="<?php echo get_stylesheet_directory_uri()?>/img/if.png" alt="informaçoes da bragantec" width="70" height="120" style="padding-right: 0px;padding-left: 0px;">
-            <div class="col-md-9">
-              IFSP - Bra: <br>
+            Contato IFSP Bragança:
+            <div class="contatoDivIf">
               Telefone: <a href="tel:1140347800" target="_blank">(11) 4034-7800</a><br>
               E-mail: <a href="mailto:braganca@ifsp.edu.br" target="_blank">braganca@ifsp.edu.br</a><br>
-              Facebook: <a href="https://www.facebook.com/ifsp.braganca.paulista/?__tn__=%2CdkC-R-R&eid=ARApuT4n3HgL3O4V7QvZFMbotO2kJieXSOUENu7hd0653M4odQI7eTkEohKNTeiceAoNtJj6LXapV5G0&hc_ref=ARQYYPJ6m2C8EmJmuWfIhR4SXYnNzyH4C8fcJjOpSwI9oK1YAyGsOeL16HTPD79Sd08&fref=gs&dti=441352152644120&hc_location=group_dialog" target="_blank">@ifsp.braganca.paulista</a><br>
+              Facebook: <a href="https://www.facebook.com/ifsp.braganca.paulista/?__tn__=%2CdkC-R-R&eid=ARApuT4n3HgL3O4V7QvZFMbotO2kJieXSOUENu7hd0653M4odQI7eTkEohKNTeiceAoNtJj6LXapV5G0&hc_ref=ARQYYPJ6m2C8EmJmuWfIhR4SXYnNzyH4C8fcJjOpSwI9oK1YAyGsOeL16HTPD79Sd08&fref=gs&dti=441352152644120&hc_location=group_dialog" target="_blank">@ifsp.braganca.paulista</a>
             </div>
           </div>
 
           <div class="col-md-4 row text-center">
 
             <h2 class="col-md-12 text-center tituloMobile">O site foi desenvolvido por:</h2>
-            <div class="col-md-6 desenvolvedoras">
+            <div class="col-md-6 col-xs-6 desenvolvedoras">
               <a href="https://www.linkedin.com/in/beatriz-miranda-ferreira/" target="_blank">
                 <div class="col-md-12 perfil">
                     <img src="<?php echo get_stylesheet_directory_uri()?>/img/beatriz.jpg" alt="Beatriz Miranda">
@@ -47,7 +43,7 @@
               </a>
             </div>
 
-            <div class="col-md-6 desenvolvedoras">
+            <div class="col-md-6  col-xs-6 desenvolvedoras">
               <a href="https://www.linkedin.com/in/laura-a-de-almeida-chaile/" target="_blank">
                 <div class="col-md-12 perfil">
                     <img src="<?php echo get_stylesheet_directory_uri()?>/img/laura.jpg" alt="Laura Chaile">
@@ -63,11 +59,9 @@
         </div>
 
         <div class="row col-md-12 apoios">
-          <div class="col-md-6 row text-sm-center">
-            <img class="col-md-3 col-xs-1 imgIfMeninas " src="<?php echo get_stylesheet_directory_uri()?>/img/ifMeninas.png" alt="Logo if Meninas" width="20" height="120">
-            <div class="col-md-9 col-xs-9">
-              <p>Esse site foi desenvolvido em parceria com o IFMeninas, grupo que apoia meninas na computação. </p><br>
-              <img src="<?php echo get_stylesheet_directory_uri();?>/img/web.svg" width="25" alt="icone referente ao site if Meninas"/>
+          <div class="col-md-6 row contato">
+            <div class="contatoIfMeninas">
+              <p>Esse site foi desenvolvido em parceria com o IFMeninas, grupo que apoia meninas na computação. </p>
               <a href="bra.ifsp.edu.br/ifmeninas/" target="_blank">bra.ifsp.edu.br/ifmeninas/</a>
             </div>
           </div>
