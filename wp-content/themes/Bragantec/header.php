205b04e5a631e1d245a78ac4f04e9d7cd071ab81
beatrizmiranda <biia.mfs@gmail.com>
2019-06-05 17:54:01 -0300

    tentando incluir a galeria

--- wp-content/themes/Bragantec/header.php
+++ wp-content/themes/Bragantec/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
    <head>
-      <title>Bootstrap Example</title>
+      <title>Bragantec</title>
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
